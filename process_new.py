c18771b1ee829145f97a7a5817f588168ac7be22
tanishk-ou <142319731+tanishk-ou@users.noreply.github.com>
2025-11-05 12:18:01 +0530

    Update process_new.py

--- process_new.py
+++ process_new.py
@@ -1,3 +1,26 @@
+# This file contains modifications from its original version.
+# The original code was part of the google-research/unprocessing repository, 
+# licensed under the Apache License, Version 2.0.
+#
+# Original Copyright (c) [2019] [Tim Brooks, et al.]
+#
+# A copy of the license is available at:
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
 """Forward processing of raw data to sRGB images.
 
 Unprocessing Images for Learned Raw Denoising
